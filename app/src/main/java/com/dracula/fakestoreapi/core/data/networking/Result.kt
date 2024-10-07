a940e26099b620c26f4104dafb196e6b330c2d3f
Hassan Mohamed <Hassan.Mohamed@derayah.com>
2024-10-08 00:43:49 +0300

    Update the Result sealed class docs

--- app/src/main/java/com/dracula/fakestoreapi/core/data/networking/Result.kt
+++ app/src/main/java/com/dracula/fakestoreapi/core/data/networking/Result.kt
@@ -3,25 +3,26 @@ package com.dracula.fakestoreapi.core.data.networking
 /**
  * A sealed interface representing a result that can either be a success or an error.
  *
- * @param D The type of the data in case of success.
+ * @param Response The type of the data in case of success.
  * @param E The type of the error in case of failure, which must extend [Error].
  */
-sealed interface Result<out D, out E : Error> {
+sealed interface Result<out Response, out E : Error> {
 	/**
 	 * Represents a successful result containing data.
 	 *
-	 * @param D The type of the data.
+	 * @param Response The type of the data.
 	 * @property data The data of the successful result.
 	 */
-	data class Success<out D>(val data: D) : Result<D, Nothing>
+	data class Success<out Response>(val data: Response) : Result<Response, Nothing>
 
 	/**
 	 * Represents an error result containing an error.
 	 *
-	 * @param E The type of the error, which must extend [com.dracula.core.domain.utils.Error].
+	 * @param Error The type of the error, which must extend [com.dracula.fakestoreapi.core.data.networking.Error].
 	 * @property error The error of the result.
 	 */
-	data class Error<out E : com.dracula.fakestoreapi.core.data.networking.Error>(val error: E) : Result<Nothing, E>
+	data class Error<out Error : com.dracula.fakestoreapi.core.data.networking.Error>(val error: Error) :
+		Result<Nothing, Error>
 }
 
 /**
